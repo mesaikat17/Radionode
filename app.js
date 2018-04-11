79a6c25b317b752b169192525a358cea1ec6eda3
Saikat Samanta <37097031+mesaikat17@users.noreply.github.com>
2018-04-11 13:03:42 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-//mongoose.connect('mongodb://admin:admin123@radio-shard-00-00-jzjem.mongodb.net:27017,radio-shard-00-01-jzjem.mongodb.net:27017,radio-shard-00-02-jzjem.mongodb.net:27017/test?ssl=true&replicaSet=Radio-shard-0&authSource=admin')
+
 mongoose.connect('mongodb://admin:admin123@radio-shard-00-00-jzjem.mongodb.net:27017,radio-shard-00-01-jzjem.mongodb.net:27017,radio-shard-00-02-jzjem.mongodb.net:27017/test?ssl=true&replicaSet=Radio-shard-0&authSource=admin', function(err) {
-    // If no error, successfully connected
     console.log(err);
   });
 const songsRouter = require('./apinodes/routes/songs');
